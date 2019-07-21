cd2fbafe329fae235565591926e5ea6f17cb939e
jonathan wolfe <jonathanwolfe@jonathans-MacBook-Pro.local>
2019-07-20 23:33:47 -0500

    made userController basics

--- EZRecipes/src/main/java/com/revature/web/UserControllerImpl.java
+++ EZRecipes/src/main/java/com/revature/web/UserControllerImpl.java
@@ -1,5 +1,19 @@
 package com.revature.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.service.UserService;
+
+@RestController
+@RequestMapping(value = "/api")
 public class UserControllerImpl implements UserController {
 
+	private UserService userService;
+
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
 }
