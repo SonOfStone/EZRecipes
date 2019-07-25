61229f7056481b7dece65a303124ed46d416a034
SonOfStone <rwbrinson3@gmail.com>
2019-07-25 10:24:32 -0500

    fixed entire project

--- EZRecipes/src/main/java/com/revature/model/Favorite.java
+++ EZRecipes/src/main/java/com/revature/model/Favorite.java
@@ -1,82 +1,82 @@
-// package com.revature.model;
+package com.revature.model;
 
-// import javax.persistence.Entity;
-// import javax.persistence.Table;
-// import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
 
-// @Entity
-// @Table(name="favorites", schema="ezrecipes")
-// public class Favorite {
-// @Id
-// private User userId;
-// private meals mealsId;
-// private String lastAccessed;
-// @Override
-// public int hashCode() {
-	// final int prime = 31;
-	// int result = 1;
-	// result = prime * result + ((lastAccessed == null) ? 0 : lastAccessed.hashCode());
-	// result = prime * result + ((mealsId == null) ? 0 : mealsId.hashCode());
-	// result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-	// return result;
-// }
-// @Override
-// public boolean equals(Object obj) {
-	// if (this == obj)
-		// return true;
-	// if (obj == null)
-		// return false;
-	// if (getClass() != obj.getClass())
-		// return false;
-	// Favorite other = (Favorite) obj;
-	// if (lastAccessed == null) {
-		// if (other.lastAccessed != null)
-			// return false;
-	// } else if (!lastAccessed.equals(other.lastAccessed))
-		// return false;
-	// if (mealsId == null) {
-		// if (other.mealsId != null)
-			// return false;
-	// } else if (!mealsId.equals(other.mealsId))
-		// return false;
-	// if (userId == null) {
-		// if (other.userId != null)
-			// return false;
-	// } else if (!userId.equals(other.userId))
-		// return false;
-	// return true;
-// }
-// @Override
-// public String toString() {
-	// return "UserRecipe [userId=" + userId + ", recipeId=" + mealsId + ", lastAccessed=" + lastAccessed + "]";
-// }
-// public User getUserId() {
-	// return userId;
-// }
-// public void setUserId(User userId) {
-	// this.userId = userId;
-// }
-// public meals getRecipeId() {
-	// return mealsId;
-// }
-// public void setRecipeId(meals recipeId) {
-	// this.mealsId = recipeId;
-// }
-// public String getLastAccessed() {
-	// return lastAccessed;
-// }
-// public void setLastAccessed(String lastAccessed) {
-	// this.lastAccessed = lastAccessed;
-// }
-// public Favorite(User userId, meals recipeId, String lastAccessed) {
-	// super();
-	// this.userId = userId;
-	// this.mealsId = mealsId;
-	// this.lastAccessed = lastAccessed;
-// }
-// public Favorite() {
-	// super();
-	TODO Auto-generated constructor stub
-// }
+//@Entity
+//@Table(name="favorites", schema="ezrecipes")
+public class Favorite {
+//@Id
+private User userId;
+private meals mealsId;
+private String lastAccessed;
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((lastAccessed == null) ? 0 : lastAccessed.hashCode());
+	result = prime * result + ((mealsId == null) ? 0 : mealsId.hashCode());
+	result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Favorite other = (Favorite) obj;
+	if (lastAccessed == null) {
+		if (other.lastAccessed != null)
+			return false;
+	} else if (!lastAccessed.equals(other.lastAccessed))
+		return false;
+	if (mealsId == null) {
+		if (other.mealsId != null)
+			return false;
+	} else if (!mealsId.equals(other.mealsId))
+		return false;
+	if (userId == null) {
+		if (other.userId != null)
+			return false;
+	} else if (!userId.equals(other.userId))
+		return false;
+	return true;
+}
+@Override
+public String toString() {
+	return "UserRecipe [userId=" + userId + ", recipeId=" + mealsId + ", lastAccessed=" + lastAccessed + "]";
+}
+public User getUserId() {
+	return userId;
+}
+public void setUserId(User userId) {
+	this.userId = userId;
+}
+public meals getRecipeId() {
+	return mealsId;
+}
+public void setRecipeId(meals recipeId) {
+	this.mealsId = recipeId;
+}
+public String getLastAccessed() {
+	return lastAccessed;
+}
+public void setLastAccessed(String lastAccessed) {
+	this.lastAccessed = lastAccessed;
+}
+public Favorite(User userId, meals recipeId, String lastAccessed) {
+	super();
+	this.userId = userId;
+	this.mealsId = mealsId;
+	this.lastAccessed = lastAccessed;
+}
+public Favorite() {
+	super();
+	// TODO Auto-generated constructor stub
+}
 
-// }
+}
--- EZRecipes/src/main/java/com/revature/model/Rating.java
+++ EZRecipes/src/main/java/com/revature/model/Rating.java
@@ -1,96 +1,96 @@
-// package com.revature.model;
+package com.revature.model;
 
-// import javax.persistence.Column;
-// import javax.persistence.Entity;
-// import javax.persistence.Table;
-// import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
 
-// @Entity
-// @Table(name = "ratings", schema = "ezrecipes")
-// public class Rating {
-	// @Id
-	// @Column(name = "userId")
-	// private User userId;
-	// @Column(name = "idMeals")
-	// private meals idMeals;
-	// @Column(name = "rating")
-	// private double rating;
+//@Entity
+//@Table(name = "ratings", schema = "ezrecipes")
+public class Rating {
+//	@Id
+//	@Column(name = "userId")
+	private User userId;
+//	@Column(name = "idMeals")
+	private meals idMeals;
+//	@Column(name = "rating")
+	private double rating;
 
-	// public Rating(User userId, meals idMeals, double rating) {
-		// super();
-		// this.userId = userId;
-		// this.idMeals = idMeals;
-		// this.rating = rating;
-	// }
+	public Rating(User userId, meals idMeals, double rating) {
+		super();
+		this.userId = userId;
+		this.idMeals = idMeals;
+		this.rating = rating;
+	}
 
-	// public Rating() {
-		// super();
-		TODO Auto-generated constructor stub
-	// }
+	public Rating() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-	// @Override
-	// public String toString() {
-		// return "Rating [userId=" + userId + ", idMeals=" + idMeals + ", rating=" + rating + "]";
-	// }
+	@Override
+	public String toString() {
+		return "Rating [userId=" + userId + ", idMeals=" + idMeals + ", rating=" + rating + "]";
+	}
 
-	// @Override
-	// public int hashCode() {
-		// final int prime = 31;
-		// int result = 1;
-		// result = prime * result + ((idMeals == null) ? 0 : idMeals.hashCode());
-		// long temp;
-		// temp = Double.doubleToLongBits(rating);
-		// result = prime * result + (int) (temp ^ (temp >>> 32));
-		// result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		// return result;
-	// }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idMeals == null) ? 0 : idMeals.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(rating);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
 
-	// @Override
-	// public boolean equals(Object obj) {
-		// if (this == obj)
-			// return true;
-		// if (obj == null)
-			// return false;
-		// if (getClass() != obj.getClass())
-			// return false;
-		// Rating other = (Rating) obj;
-		// if (idMeals == null) {
-			// if (other.idMeals != null)
-				// return false;
-		// } else if (!idMeals.equals(other.idMeals))
-			// return false;
-		// if (Double.doubleToLongBits(rating) != Double.doubleToLongBits(other.rating))
-			// return false;
-		// if (userId == null) {
-			// if (other.userId != null)
-				// return false;
-		// } else if (!userId.equals(other.userId))
-			// return false;
-		// return true;
-	// }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rating other = (Rating) obj;
+		if (idMeals == null) {
+			if (other.idMeals != null)
+				return false;
+		} else if (!idMeals.equals(other.idMeals))
+			return false;
+		if (Double.doubleToLongBits(rating) != Double.doubleToLongBits(other.rating))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
 
-	// public User getUserId() {
-		// return userId;
-	// }
+	public User getUserId() {
+		return userId;
+	}
 
-	// public void setUserId(User userId) {
-		// this.userId = userId;
-	// }
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
 
-	// public meals getIdMeals() {
-		// return idMeals;
-	// }
+	public meals getIdMeals() {
+		return idMeals;
+	}
 
-	// public void setIdMeals(meals idMeals) {
-		// this.idMeals = idMeals;
-	// }
+	public void setIdMeals(meals idMeals) {
+		this.idMeals = idMeals;
+	}
 
-	// public double getRating() {
-		// return rating;
-	// }
+	public double getRating() {
+		return rating;
+	}
 
-	// public void setRating(double rating) {
-		// this.rating = rating;
+	public void setRating(double rating) {
+		this.rating = rating;
 
-	// }
-// }
+	}
+}
